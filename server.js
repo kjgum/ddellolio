7e6e599fc0fb44c4d599af22223e74add1849744
Kristin Gumbert <kristingumbert@Kristins-MacBook-Pro.local>
2016-08-01 19:18:57 -0400

    layout work

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
+app.use(express.static(process.cwd() + '/public'));
 
 // Routes
 // =============================================================
